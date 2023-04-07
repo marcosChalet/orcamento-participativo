ad92ae29d67f24e588fef5e0ee0fab756cf4cc02
marcosChalet <chaletmarcos@gmail.com>
2023-04-07 13:35:17 -0300

    chore: adicionando relative paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            assets: './assets',
+            components: './src/components',
+          },
+        },
+      ],
+    ],
+  };
 };
