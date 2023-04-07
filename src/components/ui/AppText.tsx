7ed97a334a89cd63035f1d8da158ab16c17545e2
marcosChalet <chaletmarcos@gmail.com>
2023-04-07 16:18:16 -0300

    feat: add meu componente Text

---
+++ src/components/ui/AppText.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+type TextType = {
+  children: React.ReactNode;
+  style?: {};
+};
+
+export default function AppText({children, style}: TextType) {
+  return <Text style={[styles.baseText, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  baseText: {
+    color: '#532B1D',
+  },
+});
