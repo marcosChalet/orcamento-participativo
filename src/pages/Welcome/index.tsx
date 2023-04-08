375af9b6f6d223214fbc892e19711438f14acafe
marcosChalet <chaletmarcos@gmail.com>
2023-04-08 10:21:49 -0300

    feat: add responsividade

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, View} from 'react-native';
+import StyleSheet from 'react-native-media-query';
 
 import LogoUFCA from 'assets/imgs/ufca-logo.svg';
 import PeopleIllustration from 'assets/imgs/people-illustration.svg';
@@ -10,27 +11,35 @@ import AppText from 'components/ui/AppText';
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <LogoUFCA width={81} height={57} />
-      <AppText style={styles.helloText}>
-        Oi! Boas vindas ao app de Orçamento Participativo da UFCA!
-      </AppText>
-      <PeopleIllustration />
-      <AppText style={styles.description}>
-        Aqui você poderá participar ativamente das decisões tomadas na
-        universidade debatendo, propondo e votando em projetos e na distribuição
-        orçamentária.
-      </AppText>
-      <Button>
-        <AppText style={styles.buttonText}>Continuar</AppText>
-      </Button>
+      <ScrollView>
+        <LogoUFCA width={81} height={57} />
+        <AppText style={styles.helloText}>
+          Oi! Boas vindas ao app de Orçamento Participativo da UFCA!
+        </AppText>
+        <PeopleIllustration
+          width={styles.illustration.width}
+          height={styles.illustration.height}
+        />
+        <AppText style={styles.description}>
+          Aqui você poderá participar ativamente das decisões tomadas na
+          universidade debatendo, propondo e votando em projetos e na
+          distribuição orçamentária.
+        </AppText>
+        <Button>
+          <AppText style={styles.buttonText}>Continuar</AppText>
+        </Button>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const {styles} = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 34,
+    padding: 15,
+    '@media (min-width:400px)': {
+      padding: 34,
+    },
   },
   buttonText: {
     color: '#FFF',
@@ -41,17 +50,27 @@ const styles = StyleSheet.create({
     fontSize: 25,
     lineHeight: 26,
     fontFamily: 'Alegreya-ExtraBoldItalic',
-    maxWidth: 283,
-    width: '90%',
+    width: 270,
     paddingLeft: 2,
     marginTop: 7,
   },
   description: {
-    fontFamily: 'Roboto-Medium',
     fontSize: 16,
-    lineHeight: 15.9,
+    lineHeight: 16,
     paddingLeft: 2,
-    marginTop: 30,
+    paddingTop: 15,
     marginBottom: 24,
+    '@media (min-width:400px)': {
+      paddingBottom: 10,
+      fontSize: 18,
+    },
+  },
+  illustration: {
+    width: '70%',
+    height: 200,
+    '@media (min-width:400px)': {
+      width: 390,
+      height: 390,
+    },
   },
 });
