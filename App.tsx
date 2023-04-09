a562868106671094058d3b1ea14b9102a14c9f0d
marcosChalet <chaletmarcos@gmail.com>
2023-04-09 08:02:47 -0300

    refactor(routes): movendo NavigationContainer para o componente de rotas

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 
 import Routes from './src/routes/Routes';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
-        <Routes />
-      </SafeAreaView>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
+      <Routes />
+    </SafeAreaView>
   );
 }
 
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,22 +3,25 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Welcome from '../pages/Welcome';
 import Login from '../pages/Login';
+import {NavigationContainer} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Welcome"
-        component={Welcome}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{headerShown: false}}
-      />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
